35311bf210acd5a9056ca491860f5be00cac6233
Walter C. Pelissero <walter@pelissero.de>
2012-05-22 10:07:43 +0200

    Implemented damping of temperature readings.
    Ignore-this: bdb6ce9ea5c66729006b55f6fe6914e6
    Re-implemented turn-off delay to make it look like an hysteresis.
    More and better documentation of tunables.
    
    darcs-hash:20120522080743-1ddae-28955afce93f2b4c19530c32413e58851a9940b1

--- fans.c
+++ fans.c
@@ -23,6 +23,61 @@ Boston, MA 02111-1307, USA.*/
 #include <util/delay.h>
 #include <avr/io.h>
 
+
+// The PWM duty cycle will tell the voltage applied to a fan and thus
+// its speed.  Most motors don't even budge below half their rated
+// voltage.  Here we use a conservative 70%, but you should check your
+// fans data sheet or do some experiments.  If you find yourself using
+// to high values here, it may mean that the fans you are trying to
+// employ don't like to have their speed modulated at all; choose
+// another type.
+#ifndef MINIMUM_DUTY_CYCLE
+# define MINIMUM_DUTY_CYCLE	7/10
+#endif
+
+// How often the main loop is run.  Every 0.1s should be alright.
+#ifndef GRANULARITY
+# define GRANULARITY 100		// in ms
+#endif
+
+// How long the fan should be kept running before actually turning
+// completely off.  This off-delay will resemble a sort of hysteresis
+// to the naked eye.  The purpose is to avoid repeated on-off-on
+// transitions when the temperature is floating around the trigger
+// value.  This value is proportional to the GRANULARITY (above).
+#ifndef TRAIL_ON
+# define TRAIL_ON 50		// x * GRANULARITY
+#endif
+
+// How long to run the fan at maximum speed on start, so that it will
+// gain momentum.  This is proportional to the GRANULARITY (above).
+#ifndef SPINUP
+# define SPINUP 10		// x * GRANULARITY
+#endif
+
+//  You don't want to start the fans below this temperature.  This is
+//  actually a voltage measured on NTC_PIN.  The value is between 0
+//  and 1023, the latter being Vcc.  So, if Vcc is around 5V, here 300
+//  means something about 1.46V.  This is what you would read at about
+//  22-25 degrees centigrade.
+#ifndef MIN_TEMP
+# define MIN_TEMP 300
+#endif
+
+// Temperature variations are damped by this factor. The bigger the
+// slower the fan speed variations.  A value of 1 gets away with any
+// kind of damping.
+#ifndef DAMP_FACTOR
+# define DAMP_FACTOR 10
+#endif
+
+// end of tuning knobs
+//////////////////////////////////////////////////////////////////////
+
+
+// You may want to change the following only if you change the
+// hardware schematics.  In most cases you should leave them alone.
+
 // pins on port B
 #define FAN1_PIN PB0		// pin 5
 #define FAN2_PIN PB1		// pin 6
@@ -36,36 +91,14 @@ Boston, MA 02111-1307, USA.*/
 
 //////////////////////////////////////////////////////////////////////
 
-#define GRANULARITY 100		// in ms
-#define MINIMUM_ON 50		// x * GRANULARITY
-#define SPINUP 10		// x * GRANULARITY
-
-//  You don't want to start the fans below this temperature.  This is
-//  actually a voltage measured on NTC_PIN.  The value is between 0
-//  and 1023, the latter being Vcc.  So, if Vcc is around 5V, here 300
-//  means something about 1.46V.  This is what you would read at about
-//  22-25 degrees centigrade.
-#ifndef MIN_TEMP
-# define MIN_TEMP 300
-#endif
 
 #define HIGH 1
 #define LOW 0
 #define FULL_SPEED 255
 #define STOP 0
+#define MINIMUM_SPEED ((FULL_SPEED) * MINIMUM_DUTY_CYCLE)
 #define INPUT 1
 #define OUTPUT 0
-
-// The PWM duty cycle will tell the voltage applied to a fan and thus
-// its speed.  Most motors don't even budge below half their rated
-// voltage.  Here we use a conservative 70%, but you should check your
-// fans data sheet or do some experiments.
-#ifndef MINIMUM_DUTY_CYCLE
-# define MINIMUM_DUTY_CYCLE	7/10
-#endif
-#define MINIMUM_SPEED ((FULL_SPEED) * MINIMUM_DUTY_CYCLE)
-
-
 
 #define setRegisterBits(reg,mask,bits) ((reg) = ((reg) & ~(mask)) | ((bits) & (mask)))
 #define setPortBitsDirection(port,mask,direction) setRegisterBits(*(&(port) - 1), (mask), (((direction) == OUTPUT) ? (mask) : 0))
@@ -159,7 +192,7 @@ readAnalogPin (int adc)
   return (high << 8) | low;
 }
 
-// just link readAnalogPin, but with lower (8 bit) resolution
+// just like readAnalogPin(), but with lower (8 bit) resolution
 static unsigned
 readAnalogPinLR (int adc)
 {
@@ -228,21 +261,21 @@ class Fan
  public:
   Fan(int pin);
   void setSpeed (unsigned percent);
+  unsigned getSpeed ();
 
  private:
   int pin;
   unsigned spinup;
-  unsigned stay_on;
+  unsigned trail_on;
 };
 
 Fan::Fan (int pin_number)
 {
   pin = pin_number;
-  spinup = stay_on = 0;
+  spinup = trail_on = 0;
+  setSpeed(0);
 }
 
-
-// here speed is in the range 0-100
 void
 Fan::setSpeed (unsigned percent)
 {
@@ -254,16 +287,21 @@ Fan::setSpeed (unsigned percent)
 
       if (percent > 100)
 	percent = 100;
-      if (stay_on)
-	--stay_on;
-      if (percent == 0 && !stay_on)
-	setFANspeed(pin, STOP);
+      if (percent == 0)
+	{
+	  if (trail_on)
+	    --trail_on;
+	  else
+	    setFANspeed(pin, STOP);
+	}
       else
 	{
-	  if (getFANspeed(pin) == STOP)
+	  unsigned current_speed = getFANspeed(pin);
+
+	  trail_on = TRAIL_ON;
+	  if (current_speed == STOP)
 	    {
 	      spinup = SPINUP;
-	      stay_on = MINIMUM_ON;
 	      speed = FULL_SPEED;
 	    }
 	  else
@@ -273,9 +311,16 @@ Fan::setSpeed (unsigned percent)
     }
 }
 
+unsigned
+Fan::getSpeed ()
+{
+  return getFANspeed(pin);
+}
+
 static void
 loop ()
 {
+  unsigned old_temp = readAnalogPin(NTC_ADC);
   Fan fan1(FAN1_PIN);
   Fan fan2(FAN2_PIN);
 
@@ -286,7 +331,16 @@ loop ()
       unsigned range = readPot(POT2_ADC);
       unsigned fan1_speed, fan2_speed;
 
+      // Damp the temperature value averaging it with the old one.
+      // The more weight on the old one the less dynamic the
+      // variations.
+      temp = (((DAMP_FACTOR - 1) * old_temp) + temp) / DAMP_FACTOR;
+      old_temp = temp;
+      // Reduce the temperature offset, because we are not interested
+      // in temperatures below MIN_TEMP.
       temp -= MIN_TEMP;
+      // Scale down min and range pot values because we don't need so
+      // much value range.
       min >>= 1;
       range >>= 1;
       fan1_speed = (temp > min ? ((temp - min) * 100 / range) : 0);
