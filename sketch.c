ba50c4c19194802ef4e7c058712cf20e3a889677
Walter C. Pelissero <walter@pelissero.de>
2014-12-02 16:38:27 +0100

    Change title in source file.

--- sketch.c
+++ sketch.c
@@ -1,24 +1,23 @@
 /* -*- C++ -*- */
 
-/*  main.c --- main program
+/*  sketch.c --- AVR ATtiny45 firmware for the controller
 
- Copyright (C) 2012-2014 by Walter C. Pelissero
+    Copyright (C) 2012-2014 by Walter C. Pelissero
 
- Author: Walter C. Pelissero <walter@pelissero.de>
- Project: PC fan control
+    Author: Walter C. Pelissero <walter@pelissero.de>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2, or (at
-your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MA 02111-1307, USA.*/
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2, or (at
+    your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; see the file COPYING.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA. */
 
 #include <util/delay.h>
 #include <avr/io.h>
